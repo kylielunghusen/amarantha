529bca112a39623339d0ce24c7ef98bc392dde78
Kylie Lunghusen <kylielunghusen@gmail.com>
2017-11-15 00:27:02 +1100

    Continuous gumball updates

--- gumball/gumball.js
+++ gumball/gumball.js
@@ -1,4 +1,22 @@
-window.onload = function() {}
+window.onload = function() {
+  setInterval(handleRefresh, 3000);
+}
+
+function handleRefresh() {
+  var url = "http://gumball.wickedlysmart.com?callback=updateSales&random=" + (new Date()).getTime();
+  
+  var newScriptElement = document.createElement("script");
+  newScriptElement.setAttribute("src", url);
+  newScriptElement.setAttribute("id", "jsonp");
+  
+  var oldScriptElement = document.getElementById("jsonp");
+  var head = document.getElementByTagName("head")[0];
+  if(oldScriptElement == null) {
+    head.appendChild(newScriptElement);
+  } else {
+    head.replaceChild(newScriptElement, oldScriptElement);
+  }
+}
 
 function updateSales(sales) {
   var salesDiv = document.getElementById("sales");
